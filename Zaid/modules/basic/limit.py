310786d05d8359080c631c66d1a1e4b9997c40fc
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-06-28 14:33:40 +0700

    Update limit.py

--- Zaid/modules/basic/limit.py
+++ Zaid/modules/basic/limit.py
@@ -1,26 +1,13 @@
-"""
-if you can read this, this meant you use code from Geez | Ram Project
-this code is from somewhere else
-please dont hestitate to steal it
-because Geez and Ram doesn't care about credit
-at least we are know as well
-who Geez and Ram is
-
-
-kopas repo dan hapus credit, ga akan jadikan lu seorang developer
-
-YANG NYOLONG REPO INI TRUS DIJUAL JADI PREM, LU GAY...
-©2023 Geez | Ram Team
-"""
 import asyncio
 from pyrogram import Client, raw
 from pyrogram.types import Message
-from geezlibs.geez import geez
-from geezlibs.geez.helper.basic import edit_or_reply
-from Geez.modules.basic import add_command_help
-from Geez import cmds
 
-@geez("limit", cmds)
+from Zaid.helper.utility import edit_or_reply
+
+from Zaid.modules.help import add_command_help
+
+
+@geez("limit")
 async def spamban(client: Client, m: Message):
     await client.unblock_user("SpamBot")
     response = await client.send(
@@ -40,6 +27,6 @@ async def spamban(client: Client, m: Message):
 add_command_help(
     "limit",
     [
-        [f"{cmds}limit", "cek limit/batasan akun."],
+        [f"limit", "cek limit/batasan akun."],
     ],
 )
